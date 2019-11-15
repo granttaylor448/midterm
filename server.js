3fcf3190cae2b7da75985772ac464e5ea1c993b8
granttaylor448 <granttaylor448@gmail.com>
2019-11-15 23:38:28 +0000

    modified server

--- server.js
+++ server.js
@@ -35,11 +35,15 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const menuRoutes = require("./routes/menu");
+const ordersRoutes = require("./routes/orders");
+const menu_ordersRoutes = require("./routes/menu_orders");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/menu", menuRoutes(db));
+app.use("/api/orders", ordersRoutes(db));
+app.use("/api/menu_orders", menu_ordersRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 
