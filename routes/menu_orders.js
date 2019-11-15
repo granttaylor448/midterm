1b5ad61d72cce7e257466d70f07ae704d26221fe
granttaylor448 <granttaylor448@gmail.com>
2019-11-15 23:28:07 +0000

    add routes for orders and menu_ordrs

--- routes/menu_orders.js
+++ routes/menu_orders.js
@@ -1,21 +1,15 @@
-/*
- * All routes for Widgets are defined here
- * Since this file is loaded in server.js into api/widgets,
- *   these routes are mounted onto /widgets
- * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
- */
 
 const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    let query = `SELECT * FROM menu`;
+    let query = `SELECT * FROM menu_orders`;
     console.log(query);
     db.query(query)
       .then(data => {
-        const menu = data.rows;
-        res.json({ menu });
+        const menu_orders = data.rows;
+        res.json({ menu_orders });
       })
       .catch(err => {
         res
