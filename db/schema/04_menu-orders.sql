53ec95884393f6b32616f1de6dd6c60a32eb32ac
granttaylor448 <granttaylor448@gmail.com>
2019-11-15 22:32:41 +0000

    add schema files

---
+++ db/schema/04_menu-orders.sql
@@ -0,0 +1,6 @@
+CREATE TABLE menu_orders (
+  id SERIAL PRIMARY KEY NOT NULL,
+  menu_id INTEGER REFERENCES menu(id) ON DELETE CASCADE,
+  order_id INTEGER REFERENCES orders(id) ON DELETE CASCADE,
+  quantity INT
+);
