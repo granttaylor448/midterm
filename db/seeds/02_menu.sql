ac841818ee8336251e8feb47bdf1cafe2aaed6b7
granttaylor448 <granttaylor448@gmail.com>
2019-11-15 22:29:47 +0000

    add seeds and schema

---
+++ db/seeds/02_menu.sql
@@ -0,0 +1,5 @@
+INSERT INTO menu (name, description, photo, price)
+VALUES('burger', 'classic burger', 'url', 500),
+('salmon', 'atlantic salmon', 'url', 500),
+('sandwich', 'may be from tim hortons', 'url', 500),
+('steak', 'alberta beef', 'url', 500);
