f8a8633c85c8d3b3753ebeb0dd0a3fabade1b6f3
granttaylor448 <granttaylor448@gmail.com>
2019-11-15 23:02:17 +0000

    dropped widgets

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,6 +1,7 @@
 -- Drop and recreate Users table (Example)
 
 DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS widgets cascade;
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
